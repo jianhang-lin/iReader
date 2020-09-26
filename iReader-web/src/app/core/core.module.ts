53b33510dc4925c744e14229c1d23f70ad6354fd
JianhangLin <linjianhang1010@gmail.com>
2020-09-26 16:37:30 +0800

    feat(app): define app html layout by use footer and header

--- iReader-web/src/app/core/core.module.ts
+++ iReader-web/src/app/core/core.module.ts
@@ -1,13 +1,18 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeadComponent } from './head/head.component';
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [HeadComponent, FooterComponent, SidebarComponent],
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ]
 })
 export class CoreModule {
