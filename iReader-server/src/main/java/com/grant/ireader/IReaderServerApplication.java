3a1754d4c3e24762356d888ca597c343c6e3e9a1
JianhangLin <linjianhang1010@gmail.com>
2020-09-26 16:04:41 +0800

    Init iReader server

---
+++ iReader-server/src/main/java/com/grant/ireader/IReaderServerApplication.java
@@ -0,0 +1,13 @@
+package com.grant.ireader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IReaderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IReaderServerApplication.class, args);
+    }
+
+}
---
+++ iReader-server/src/test/java/com/grant/ireader/IReaderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grant.ireader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IReaderServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
